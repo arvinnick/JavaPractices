001e8e391efe445356d7d094bf805420ab64b155
Mammad Mortaji <mortajiupwork@gmail.com>
2021-11-06 18:08:16 +0400

    "trying to" trying to change terminal based input into GUI based.

---
+++ broCode/src/ScanningUserInput.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class ScanningUserInput {
+
+    public static void main(String[] args){
+
+        //getting the user's input by using a dialog box.
+        String name = JOptionPane.showInputDialog("Please let me know your name");
+        //showing the message alongside with the user input data in a message box.
+        JOptionPane.showMessageDialog(null, "Hi "+name+"!");
+
+        int year = Integer.parseInt(JOptionPane.showInputDialog("When did you born?"));
+        JOptionPane.showMessageDialog(null, "you have been born at "+year+".");
+
+        String food = JOptionPane.showInputDialog("what delicious do you like?");
+        JOptionPane.showMessageDialog(null, "You like "+food+".");
+
+
+    }
+}
