ddd3af4d55042234a9e8c4159ed99a09c54d6f51
Mammad Mortaji <mortajiupwork@gmail.com>
2021-11-07 13:38:05 +0400

    Methodizing previous version

--- broCode/src/ScanningUserInput.java
+++ broCode/src/ScanningUserInput.java
@@ -4,17 +4,27 @@ import javax.swing.JOptionPane;
 
 public class ScanningUserInput {
 
+    public static String s_question(String q){
+        String answer = JOptionPane.showInputDialog(q);
+        return answer;
+    }
+    public static Integer i_question(String q){
+        String answer = JOptionPane.showInputDialog(q);
+        Integer i_answer = Integer.parseInt(answer);
+        return i_answer;
+    }
+
     public static void main(String[] args){
 
         //getting the user's input by using a dialog box.
-        String name = JOptionPane.showInputDialog("Please let me know your name");
+        String name = s_question("Please let me know your name");
         //showing the message alongside with the user input data in a message box.
         JOptionPane.showMessageDialog(null, "Hi "+name+"!");
 
-        int year = Integer.parseInt(JOptionPane.showInputDialog("When did you born?"));
+        Integer year = i_question("When did you born?");
         JOptionPane.showMessageDialog(null, "you have been born at "+year+".");
 
-        String food = JOptionPane.showInputDialog("what delicious do you like?");
+        String food = s_question("what delicious do you like?");
         JOptionPane.showMessageDialog(null, "You like "+food+".");
 
         Random random = new Random();
