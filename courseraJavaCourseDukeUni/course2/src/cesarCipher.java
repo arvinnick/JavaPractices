ed9b5608794e9533e637a909ec50dcafbc3d4046
Arvin Nick <arvin.nick.90@gmail.com>
2022-08-19 18:55:57 +0400

    cesar cipher passed the test

--- courseraJavaCourseDukeUni/course2/src/cesarCipher.java
+++ courseraJavaCourseDukeUni/course2/src/cesarCipher.java
@@ -1,60 +1,77 @@
 import edu.duke.FileResource;
 
-import java.util.Locale;
+import java.nio.file.LinkPermission;
+
 
 public class cesarCipher {
-
-    static String encrypt(String input, int key){
-
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    
+    static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static String stringShifter(int key){
+        String ret;
+        //write the alphabet
+        //take the tail (substrig by key)
         String tail = alphabet.substring(key);
-        StringBuilder shiftedtedAlphabet = new StringBuilder(tail+alphabet);
-        String shiftedtedAlphabetString = shiftedtedAlphabet.toString();
-        shiftedtedAlphabetString = shiftedtedAlphabetString.substring(0,alphabet.length());
-        StringBuilder inputBuilder = new StringBuilder(input);
-        for (int i = 0; i < input.length(); i++){
+        //take the head (substring begin with 0 and end with key)
+        String head = alphabet.substring(0,key);
+        //add the tail to the head
+        ret = tail + head;
+        return ret;
+    }
+    static String encrypt(String input, int key){
+        //initialize the return
+        String ret = "";
+        //build the shiftedAlphabet
+        String shiftedAlphabet = stringShifter(key);
+        //for charachter in input:
+        for (int i = 0; i < input.length(); i++) {
+            //turn the charachter into upper case
             char ch = input.charAt(i);
-            //check if the charachter is up. case or lo. case
-            boolean up = Character.isUpperCase(ch);
-            if (up){
-                ch = Character.toLowerCase(ch);
+            boolean lowerCase = Character.isLowerCase(ch);
+            ch = Character.toUpperCase(ch);
+            //find the index of the charachter in
+            int index = alphabet.indexOf(ch);
+            char shiftCh = ' ';
+            try {
+                //find the charachter at the index in shifter
+                shiftCh = shiftedAlphabet.charAt(index);
+            } catch (StringIndexOutOfBoundsException e){
+                shiftCh = ch;
             }
-            if (alphabet.indexOf(ch) != -1) {
-                //find the index of the char in alphabet
-                int indexInAlphabet = alphabet.indexOf(ch);
-                //find the char in the shifted alphabet for the same index
-                char charAtShiftedAlphabet = shiftedtedAlphabetString.charAt(indexInAlphabet);
-                //replace the char in input with the char of the shifted
-                if (up){
-                    charAtShiftedAlphabet = Character.toUpperCase(charAtShiftedAlphabet);
+            finally {
+                if(lowerCase){
+                    shiftCh = Character.toLowerCase(shiftCh);
                 }
-                inputBuilder.setCharAt(i, charAtShiftedAlphabet);
-            }
-            else{
-                inputBuilder.setCharAt(i, ch);
+                //add the charachter to the return
+                ret = ret + shiftCh;
             }
         }
-        return inputBuilder.toString();
+        return ret;
     }
     static void testEncrypt(){
         System.out.println(encrypt("FIrST LEgION ATTACK EAST FLANK!",
                 23).equals("CFoPQ IBdFLK XQQXZH BXPQ CIXKH!"));
     }
 
-    static void testCaesar(){
+    static void testCaesar() {
         FileResource fr = new FileResource();
         String message = fr.asString();
         String encrypted = encrypt(message, 12);
         System.out.println("key is " + 12 + "\n" + encrypted);
     }
-
-    static String encryptTwoKeys(String input, int key1, int key2){
-
-        return "";
-    }
+    static void testStringShifter(){
+        if(stringShifter(12).equals("MNOPQRSTUVWXYZABCDEFGHIJKL")){
+            System.out.println("test 1 passed");
+        }
+        if(stringShifter(13).equals("NOPQRSTUVWXYZABCDEFGHIJKLM")) {
+            System.out.println("test 2 passed");
+        }
+            if (stringShifter(20).equals("UVWXYZABCDEFGHIJKLMNOPQRST")) {
+                System.out.println("test 2 passed");
+            }
+        }
 
     public static void main(String[] arg){
-        testCaesar();
+        testEncrypt();
     }
 
 
